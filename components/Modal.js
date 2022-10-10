6037a78e316ff186dc076fcf0a94bc0ff03fa4ca
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-10-10 10:26:47 -0400

    added v9 configuration, configured Modal.js to create Post, Add image to DB with caption, get ID of newly created image

--- components/Modal.js
+++ components/Modal.js
@@ -3,8 +3,19 @@ import { modalState } from '../atoms/modalAtom';
 import { Dialog, Transition } from '@headlessui/react';
 import { Fragment, useRef, useState } from 'react';
 import { CameraIcon } from '@heroicons/react/outline';
+import { db, storage } from '../firebase';
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  updateDoc,
+  doc,
+} from 'firebase/firestore';
+import { useSession } from 'next-auth/react';
+import { ref, getDownloadURL, uploadString } from 'firebase/storage';
 
 function Modal() {
+  const { data: session } = useSession();
   const [open, setOpen] = useRecoilState(modalState);
   const filePickerRef = useRef(null);
   const [loading, setLoading] = useState(false);
@@ -14,6 +25,34 @@ function Modal() {
   const uploadPost = async () => {
     if (loading) return;
     setLoading(true);
+
+    // Create a post and add to firestore posts collection
+    // Get the post ID for the newly created post
+    // uplaod the image to firebase storage with the post ID
+    // get a download URL from fb storage and update original post with image
+
+    const docRef = await addDoc(collection(db, 'posts'), {
+      username: session.user.username,
+      caption: captionRef.current.value,
+      profileImg: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+
+    console.log('New doc added with ID', docRef.id);
+
+    const imageRef = ref(storage, `posts/${docRef.id}/image`);
+
+    await uploadString(imageRef, selectedFile, 'data_url').then(
+      async (snapshot) => {
+        const downloadURL = await getDownloadURL(imageRef);
+        await updateDoc(doc(db, 'posts', docRef.id), {
+          image: downloadURL,
+        });
+      }
+    );
+    setOpen(false);
+    setLoading(false);
+    setSelectedFile(null);
   };
 
   const addImageToPost = (e) => {
@@ -115,12 +154,13 @@ function Modal() {
                 <div className="mt-5 sm:mt-6">
                   <button
                     type="button"
-                    // disabled={!selectedFile}
+                    disabled={!selectedFile}
+                    onClick={uploadPost}
                     className="
                     inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 
                     focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300"
                   >
-                    Upload Post
+                    {loading ? 'Uploading...' : 'Upload Post'}
                   </button>
                 </div>
               </div>
